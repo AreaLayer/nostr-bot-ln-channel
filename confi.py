8b25a302da6b57edf4e231e07547ef43eb280467
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2024-07-28 10:33:16 -0300

    Update confi.py

---
+++ confi.py
@@ -0,0 +1,11 @@
+# config.py
+LND_GRPC_HOST = 'localhost:10009'
+LND_MACAROON_PATH = 'path/to/macaroon'
+LND_TLS_CERT_PATH = 'path/to/tls.cert'
+
+NOSTR_RELAYS = [
+    'wss://relay.nostr.info',
+    'wss://nostr-pub.wellorder.net'
+]
+
+PRIVATE_KEY_HEX = 'your_nostr_private_key'
