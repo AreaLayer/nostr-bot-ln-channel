74b8dc76476810392b310f5132e83681de1e72b5
223880 <rafael.segala45@gmail.com>
2024-09-16 06:10:32 -0300

    Fix

--- cln_plugin.py
+++ cln_plugin.py
@@ -23,34 +23,28 @@ def bye(plugin, name, **kwargs):
     return "Bye {}".format(name)
 
 
-@plugin.init()
 def init(options, configuration, plugin, **kwargs):
     plugin.log("Plugin helloworld.py initialized")
 
 
-@plugin.subscribe("connect")
 def on_connect(plugin, id, address, **kwargs):
     plugin.log("Received connect event for peer {}".format(id))
 
 
-@plugin.subscribe("disconnect")
 def on_disconnect(plugin, id, **kwargs):
     plugin.log("Received disconnect event for peer {}".format(id))
 
 
-@plugin.subscribe("invoice_payment")
 def on_payment(plugin, invoice_payment, **kwargs):
     plugin.log("Received invoice_payment event for label {label}, preimage {preimage},"
                " and amount of {msat}".format(**invoice_payment))
 
 
-@plugin.subscribe("invoice_creation")
 def on_invoice_creation(plugin, invoice_creation, **kwargs):
     plugin.log("Received invoice_creation event for label {label}, preimage {preimage},"
                " and amount of {msat}".format(**invoice_creation))
 
 
-@plugin.hook("htlc_accepted")
 def on_htlc_accepted(onion, htlc, plugin, **kwargs):
     plugin.log('on_htlc_accepted called')
     time.sleep(20)
