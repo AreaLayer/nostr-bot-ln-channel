e854e7ce9bf7e64d70560919b3fcabddfc4a4519
22388o <22388o@users.noreply.github.com>
2024-12-16 06:25:54 -0300

    Fix

--- ldk.py
+++ ldk.py
@@ -4,7 +4,18 @@ import channel
 # Connect peers
 def connect(peer):
     pass
-
 # Create channels
+def create_channel(peer):
+    pass
 
 # Create transactions
+def create_transaction(peer):
+    pass
+
+# Send transactions
+def send_transaction(peer):
+    pass
+
+# Receive transactions
+def receive_transaction(peer):
+    pass
